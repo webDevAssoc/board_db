f2d7b10bd73f58f295bab64b6a5d61d3240cd0fc
GDmail <GDmail91@gmail.com>
2015-10-07 05:09:07 +0900

    <게시글 읽기> 수정

--- board_read.php
+++ board_read.php
@@ -3,15 +3,43 @@
 	session_save_path("./session");
 	session_start();
 	if (!$_SESSION['is_login']) {
-		header("Location: /phplec/loginform.php");
+		header("Location: /phplec/board_db/loginform.php");
 		exit;
 	} 
 ?>
-<!-- 로그아웃 버튼 -->
-<a type="button" href="logout.php">로그아웃</a>
-<hr>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+</head>
+<body>
+	<!-- 로그아웃 버튼 -->
+	<a type="button" href="logout.php">로그아웃</a>
+	<hr>
 	
 <?
+	// DB 접속
+	mysql_connect('localhost', 'phpadmin', 'phpadmin');
+	mysql_select_db('phplec_board');
+
+	// 데이터 가져오기
+	$sql = 'SELECT id, password, title, context, auther, created 
+		FROM board_table 
+		WHERE id='.$_GET['file'];
+	$list_result = mysql_query($sql);
+
+	// 쿼리 실행 결과를 가져옴
+	$board = mysql_fetch_assoc($list_result);
+
+	if ($board != null) {
+		$list = "<h3>".$board['title']."</h3>";
+		$list .= "<b>글번호</b> : ".$board['id']."<br>";
+		$list .= "<b>글쓴이</b> : ".$board['auther']."<br>";
+		$list .= "<b>본문</b><br><p>".$board['context']."</p>";
+	}
+	echo $list;
+
+	/*
 	// GET으로 넘어온 파일명 지정
 	$file_name = "board/".$_GET['file'];
 	
@@ -25,8 +53,11 @@
 		
 		fclose($file);
 	}
+	*/
 ?>
-<br><br>
-<a type="button" href="board_del.php?file=<?=$_GET['file']?>">삭제하기</a>
-<hr>
-<a type="button" href="board.php">게시판 가기</a>
+	<br><br>
+	<a type="button" href="board_pw.php?file=<?=$_GET['file']?>">삭제하기</a>
+	<hr>
+	<a type="button" href="board.php">게시판 가기</a>
+</body>
+</html>
