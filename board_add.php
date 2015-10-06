d4cba43277bc4793710316732221948c2dde5146
GDmail <GDmail91@gmail.com>
2015-10-07 05:07:14 +0900

    <게시글 추가> 수정

--- board_add.php
+++ board_add.php
@@ -3,12 +3,38 @@
 	session_save_path("./session");
 	session_start();
 	if (!$_SESSION['is_login']) {
-		header("Location: /phplec/loginform.php");
+		header("Location: /phplec/board_db/loginform.php");
 		exit;
 	} 
 ?>
-
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+</head>
+<body>
 <?	
+	// DB 접속
+	mysql_connect('localhost', 'phpadmin', 'phpadmin');
+	mysql_select_db('phplec_board');
+
+	// 데이터 삽입하기
+	$sql = 
+	"INSERT INTO board_table (password, title, context, auther, created) 
+	VALUES (	'".$_POST['pw']."',
+			'".mysql_real_escape_string($_POST['title'])."', 
+			'".mysql_real_escape_string($_POST['context'])."',
+			'".$_SESSION['nickname']."',
+			now() )"; 
+	$result = mysql_query($sql);
+
+	// 삽입 결과
+	if ($result) {
+		echo "게시글 저장 완료";
+	} else {
+		echo "에러발생";
+	}
+	/*
 	// 파일 저장 위치 지정
 	$file_name = "board/".$_POST['title']."_by_".$_SESSION['nickname'].".txt";
 	
@@ -22,6 +48,9 @@
 	}
 	// 파일 포인터 닫음
 	fclose($file);
+	*/
 ?>
-<hr>
-<a type="button" href="board.php">게시판 가기</a>
+	<hr>
+	<a type="button" href="board.php">게시판 가기</a>
+</body>
+</html>
